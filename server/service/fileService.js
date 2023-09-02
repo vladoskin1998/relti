f3b1a64822bfd4cafd37b87d84823444d2b31495
vlados <vladosik4891@gmail.com>
2023-09-02 11:43:21 +0300

    path add to fileservice

--- server/service/fileService.js
+++ server/service/fileService.js
@@ -2,11 +2,13 @@ import * as uuid from 'uuid';
 import * as path from 'path';
 import fs from 'fs'
 
+const __dirname = path.resolve()
+
 class FileService {
     saveFile(image) {
         try {
             const fileName = uuid.v1() + '.png'
-            const dir = path.resolve('images', fileName)
+            const dir = path.join(__dirname,'images', fileName)
             image.mv(dir)
             return fileName
         } catch (error) {
@@ -14,7 +16,7 @@ class FileService {
         }
     }
     deleteFile(fileName) {
-        const dir = path.resolve('images', fileName)
+        const dir = path.join(__dirname,'images', fileName)
         fs.unlink(dir, (e) => console.log(e))
     }
 
